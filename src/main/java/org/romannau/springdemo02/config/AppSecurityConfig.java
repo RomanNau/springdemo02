96dc9ea86f1309ecfa5b0dc5c6b45f0a31db8c1b
RomanNaun <romoolay@gmail.com>
2022-05-14 16:56:45 +0300

    3.0.4+;

--- src/main/java/org/romannau/springdemo02/config/AppSecurityConfig.java
+++ src/main/java/org/romannau/springdemo02/config/AppSecurityConfig.java
@@ -2,7 +2,7 @@ package org.romannau.springdemo02.config;
 
 
 import org.springframework.context.annotation.Configuration;
-import org.springframework.security.config.annotation.WebSecurityConfigurerAdapter
+import org.springframework.security.config.annotation.WebSecurityConfigurerAdapter;
 
 @Configuration
 public class AppSecurityConfig extends WebSecurityConfigurerAdapter {
