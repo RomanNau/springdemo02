1f4e516afb6ced852cf00a5671dbcf5d74649350
RomanNaun <romoolay@gmail.com>
2022-05-14 16:35:52 +0300

    Initial commit

---
+++ src/main/java/org/romannau/springdemo02/config/AppSecurityConfig.java
@@ -0,0 +1,9 @@
+package org.romannau.springdemo02.config;
+
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.WebSecurityConfigurerAdapter
+
+@Configuration
+public class AppSecurityConfig extends WebSecurityConfigurerAdapter {
+}
---
+++ src/main/java/org/romannau/springdemo02/controllers/AppController.java
@@ -0,0 +1,19 @@
+package org.romannau.springdemo02.controllers;
+
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class AppController {
+
+    @GetMapping
+    public String index() {
+        return  "index";
+    }
+
+    @GetMapping("/authenticated")
+    public String au() {
+        return  "authenticated";
+    }
+}
